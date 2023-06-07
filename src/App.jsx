d650f77ead4ed40aea374aeba496a38cbc37058a
Fidan Haliti <fidan91haliti@gmail.com>
2023-06-07 11:41:00 +0200

    localStorage itegration

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-
-import './App.css'
-import WorkerList from './assets/components/Workers/WorkerList'
-import Workers from './assets/components/Workers/Workers'
-import { useState } from 'react'
+import "./App.css";
+import WorkerList from "./assets/components/Workers/WorkerList";
+import Workers from "./assets/components/Workers/Workers";
+import { useEffect, useState } from "react";
 
 function App() {
-const [workers, setWorkers] = useState([])
+  const [workers, setWorkers] = useState(
+    localStorage.getItem("workers") ? JSON.parse(localStorage.getItem("workers")) : []
+  );
+  useEffect(() => {
+    localStorage.setItem("workers", JSON.stringify(workers));
+  }, [workers]);
 
   return (
-    <div className="app-card">  
-    <h1 className='mt-6 text-center text-white text-3xl'>Automation salary</h1>
-      <Workers setWorkers={setWorkers}/>
-      <WorkerList workers={workers} setWorkers={setWorkers}/>
+    <div className="app-card">
+      <h1 className="mt-6 text-center text-white text-3xl">
+        Automation salary
+      </h1>
+      <Workers setWorkers={setWorkers} />
+      <WorkerList workers={workers} setWorkers={setWorkers} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
